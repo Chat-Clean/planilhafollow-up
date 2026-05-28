b725816f7062005317a1e2ae3bcf2d76c5f9a28f
Chat-Clean <desmistificandovendas.av@gmail.com>
2026-05-28 10:58:26 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Script from "next/script"; // Importação necessária
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -21,26 +21,34 @@ export default function RootLayout({
           <img 
             height="1" 
             width="1" 
-            style={{ "display:none" }}
+            style={{ display: "none" }} 
             src="https://www.facebook.com/tr?id=1773986837313693&ev=PageView&noscript=1"
+            alt="" 
           />
         </noscript>
       </head>
       <body>
         {children}
-        {/* Script do Meta Pixel */}
-        <script>
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '1773986837313693');
-          fbq('track', 'PageView');
-        </script>
+
+        {/* Script do Meta Pixel usando o componente nativo do Next.js */}
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1773986837313693');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
       </body>
     </html>
   );
