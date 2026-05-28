efe476535fb1011e9a165a6d68e2e9fc552b9cf6
Chat-Clean <desmistificandovendas.av@gmail.com>
2026-05-28 10:55:27 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,32 +18,29 @@ export default function RootLayout({
       <head>
         {/* Fallback para quando o usuário está com JavaScript desativado */}
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1574507636964121&ev=PageView&noscript=1"
+          <img 
+            height="1" 
+            width="1" 
+            style={{ "display:none" }}
+            src="https://www.facebook.com/tr?id=1773986837313693&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
       <body>
         {children}
-
         {/* Script do Meta Pixel */}
-        <Script id="fb-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1574507636964121');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1773986837313693');
+          fbq('track', 'PageView');
+        </script>
       </body>
     </html>
   );
